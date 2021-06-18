7847bebc4b5120b9f95c3b14178400cf22e05f1d
emieuxcharles <emieuxcharles@gmail.com>
2021-06-18 16:58:27 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,34 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBK1pgka5aIREqcYTGkZSSTOCt_dPLcUR4',
+  authDomain: 'supinfo4proj-93d6d.firebaseapp.com',
+  projectId: 'supinfo4proj-93d6d',
+  storageBucket: 'supinfo4proj-93d6d.appspot.com',
+  messagingSenderId: '357881862314',
+  appId: '1:357881862314:web:80e21e468b1f4c057218d3',
+  measurementId: 'G-BWSD047L54'
+};
+
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,91 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { ToastController } from '@ionic/angular';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+
+
 })
 export class HomePage {
 
-  constructor() {}
+  items: Observable<any[]>;
+
+  test:string;
+
+  dataUser = {
+    email: '',
+    password: ''
+  };
+
+  connecterUserEmail: string;
+  connectedUserUid: string;
+
+  connected:boolean;
+
+  dataReceive;
+
+  constructor(public afDB: AngularFireDatabase, public afAuth: AngularFireAuth, public toastController: ToastController, db: AngularFireDatabase) {
+    this.afAuth.authState.subscribe(auth => {
+      if (!auth) {
+        console.log('non connecté');
+        this.connected = false;
+      } else {
+        console.log("connecté");
+        this.connected = true;
+        this.connecterUserEmail = auth.email;
+        this.connectedUserUid = auth.uid;
+
+      }
+    })
+    this.items = db.list('Users').valueChanges()
+  }
+
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'Your text have been send.',
+      duration: 2000,
+    });
+    toast.present();
+  }
+
+  add(test) {
+    this.afDB.list('Users').push({
+      text: test,
+      fromUser: this.connectedUserUid
+    });
+    this.test = '';
+    this.presentToast();
+  }
+  
+
+  login(){
+    console.log(this.dataUser.email);
+    console.log(this.dataUser.password);
+    this.afAuth.signInWithEmailAndPassword(this.dataUser.email, this.dataUser.password);
+    this.dataUser = {
+      email: '',
+      password: ''
+    };
+  }
+
+  signUp(){
+    this.afAuth.createUserWithEmailAndPassword(this.dataUser.email, this.dataUser.password);
+    this.dataUser = {
+      email: '',
+      password: ''
+    };
+  }
+
+  logout(){
+    this.afAuth.signOut();
+  }
+
+  
 
 }
